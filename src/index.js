770fa52703a07878ce372aa66ca6237892c3a738
volodymyr <volodymyr.dashkevychit@gmail.com>
2023-06-12 23:20:42 +0300

    basic setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express          from 'express';
+import cors             from 'cors';
+import { CORS_OPTIONS } from './constants';
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.options('*', cors(CORS_OPTIONS));
+app.use(cors(CORS_OPTIONS));
+
+app.get('/ping', (req, res, next) => {
+	res.json('Dogshouseservice.Version1.0.1');
+});
+
+app.listen(port, () => {
+	console.log(`⚡️[server]: Server is running at port:${port}!`);
+});
